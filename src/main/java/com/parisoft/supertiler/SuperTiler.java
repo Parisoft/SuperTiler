0013a8a402d5c06b13ab858df17a0e653dadc4b3
Andre Paris <andreparis@gmail.com>
2020-11-17 15:57:28 -0300

    palette extract

--- src/main/java/com/parisoft/supertiler/SuperTiler.java
+++ src/main/java/com/parisoft/supertiler/SuperTiler.java
@@ -7,12 +7,19 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 @SuppressWarnings("Duplicates")
 public class SuperTiler {
@@ -88,7 +95,8 @@ public class SuperTiler {
             System.exit(1);
         }
 
-        WritableRaster img = ImageIO.read(new File(namespace.getString("input"))).getRaster();
+        BufferedImage input = ImageIO.read(new File(namespace.getString("input")));
+        WritableRaster img = input.getRaster();
         List<Frame> frames = new ArrayList<>();
 
         for (int y = 0; y < img.getHeight(); y += metatileHeight) {
@@ -136,6 +144,28 @@ public class SuperTiler {
                     frames.get(i).write(output);
                 }
             }
+        }
+
+        String palette = namespace.getString("palette");
+
+        if (palette != null) {
+            IndexColorModel colorModel = (IndexColorModel) input.getColorModel();
+            int[] rgb32 = new int[colorModel.getMapSize()];
+            byte[] rgb15 = new byte[rgb32.length * 2];
+
+            colorModel.getRGBs(rgb32);
+
+            for (int i = 0; i < rgb32.length; i++) {
+                int c = rgb32[i];
+                int r = (c & 0x00f80000) >> 19;
+                int g = (c & 0x0000f800) >> 6;
+                int b = (c & 0x000000f8) << 7;
+                int color = b | g | r;
+                rgb15[2 * i] = (byte) (color & 0xff);
+                rgb15[2 * i + 1] = (byte) (color >> 8);
+            }
+
+            Files.write(Paths.get(palette), rgb15, CREATE, TRUNCATE_EXISTING);
         }
 
         System.out.printf("Frames=%d%n", frames.size());
