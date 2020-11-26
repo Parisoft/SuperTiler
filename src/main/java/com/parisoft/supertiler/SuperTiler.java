95c6b7d6d1d26240c95d976eb76b58be4f6f7270
Andre Paris <andreparis@gmail.com>
2020-11-26 07:57:03 -0300

    commands

--- src/main/java/com/parisoft/supertiler/SuperTiler.java
+++ src/main/java/com/parisoft/supertiler/SuperTiler.java
@@ -7,7 +7,6 @@ import com.parisoft.supertiler.pojo.Tile;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.Namespace;
-import net.sourceforge.argparse4j.inf.Subparser;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -57,37 +56,7 @@ public class SuperTiler {
                 .build()
                 .defaultHelp(true)
                 .description("Tile generation toolkit for NES and Super NES");
-        Subparser sprite = parser.addSubparsers()
-                .addParser("sprite", true)
-                .description("Generates " + ARG_TILESET + ", " + ARG_PALETTE + "s, and " + ARG_METATILE + "s from image")
-                .setDefault("cmd", new SpriteCmd());
-        sprite.addArgument("-i", "--" + ARG_INPUT).nargs("?").required(true).type(String.class).help("Input indexed PNG image");
-        sprite.addArgument("-t", "--" + ARG_TILESET).nargs("?").required(false).type(String.class).help("Output " + ARG_TILESET + " file");
-        sprite.addArgument("-p", "--" + ARG_PALETTE).nargs("?").required(false).type(String.class).help("Output " + ARG_PALETTE + " file");
-        sprite.addArgument("-m", "--" + ARG_METATILE).nargs("?").required(false).type(String.class).help("Prefix for " + ARG_METATILE + " files");
-        sprite.addArgument("-M", "--" + ARG_MODE).nargs("?").required(false).type(String.class).choices("snes", "nes").setDefault("snes").help("Mode for target console");
-        sprite.addArgument("-B", "--" + ARG_BPP).nargs("?").required(false).type(Integer.class).choices(2, 4, 8).setDefault(4).help("Depth or number of colors per pixel");
-        sprite.addArgument("-W", "--" + ARG_METAW).nargs("?").required(false).type(Integer.class).setDefault(8).help("Metatile width in pixels");
-        sprite.addArgument("-H", "--" + ARG_METAH).nargs("?").required(false).type(Integer.class).setDefault(8).help("Metatile height in pixels");
-        sprite.addArgument("-s", "--" + ARG_TILESIZE).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3, 4, 5).setDefault(0)
-                .help("Size of the sprites (SNES only):" + System.lineSeparator()
-                              + "0 =  8x8  and 16x16 sprites" + System.lineSeparator()
-                              + "1 =  8x8  and 32x32 sprites" + System.lineSeparator()
-                              + "2 =  8x8  and 64x64 sprites" + System.lineSeparator()
-                              + "3 = 16x16 and 32x32 sprites" + System.lineSeparator()
-                              + "4 = 16x16 and 64x64 sprites" + System.lineSeparator()
-                              + "5 = 32x32 and 64x64 sprites");
-        sprite.addArgument("-S", "--" + ARG_APPLYSMALL).nargs("?").required(false).type(Boolean.class).setDefault(true)
-                .help("If " + ARG_APPLYLARGE + " is not set, all tiles size are the one define in tileize. See applylarge when both are set. (SNES only)");
-        sprite.addArgument("-L", "--" + ARG_APPLYLARGE).nargs("?").required(false).type(Boolean.class).setDefault(false)
-                .help("When " + ARG_MODE + " is snes, SuperTiler will first scan for large tiles defined in " + ARG_TILESIZE + " then, if " + ARG_APPLYSMALL + " is set, it will replace the large tile for N small tiles if N <= large/small" + System.lineSeparator()
-                              + "When " + ARG_MODE + " is nes, tiles are 8x16");
-        sprite.addArgument("--" + ARG_PALNUM).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3, 4, 5, 6, 7).setDefault(0).help("Object " + ARG_PALETTE + " number");
-        sprite.addArgument("--" + ARG_PRIORITY).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3).setDefault(2).help("Object " + ARG_PRIORITY + " (SNES only)");
-        sprite.addArgument("--" + ARG_TILESETNUM).nargs("?").required(false).type(Integer.class).choices(0, 1).setDefault(0).help("Object " + ARG_TILESET + " number (SNES only)");
-        sprite.addArgument("-X", "--" + ARG_XOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object X offset");
-        sprite.addArgument("-Y", "--" + ARG_YOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object Y offset");
-        sprite.addArgument("-T", "--" + ARG_TILEOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object tile offset");
+        SpriteCmd.create(parser);
 
         namespace = parser.parseArgsOrFail(args);
         mode = namespace.getString(ARG_MODE);
@@ -106,7 +75,7 @@ public class SuperTiler {
         input = ImageIO.read(new File(namespace.getString(ARG_INPUT)));
 
         try {
-            ((Cmd)namespace.get("cmd")).execute();
+            ((Cmd) namespace.get("cmd")).execute();
         } catch (Exception e) {
             System.err.println("ERROR: ".concat(e.getMessage()));
         }
--- src/main/java/com/parisoft/supertiler/cmd/SpriteCmd.java
+++ src/main/java/com/parisoft/supertiler/cmd/SpriteCmd.java
@@ -3,6 +3,8 @@ package com.parisoft.supertiler.cmd;
 import com.parisoft.supertiler.SuperTiler;
 import com.parisoft.supertiler.pojo.Frame;
 import com.parisoft.supertiler.pojo.Tile;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Subparser;
 
 import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
@@ -14,9 +16,23 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.parisoft.supertiler.SuperTiler.ARG_APPLYLARGE;
+import static com.parisoft.supertiler.SuperTiler.ARG_APPLYSMALL;
+import static com.parisoft.supertiler.SuperTiler.ARG_BPP;
+import static com.parisoft.supertiler.SuperTiler.ARG_INPUT;
+import static com.parisoft.supertiler.SuperTiler.ARG_METAH;
 import static com.parisoft.supertiler.SuperTiler.ARG_METATILE;
+import static com.parisoft.supertiler.SuperTiler.ARG_METAW;
+import static com.parisoft.supertiler.SuperTiler.ARG_MODE;
 import static com.parisoft.supertiler.SuperTiler.ARG_PALETTE;
+import static com.parisoft.supertiler.SuperTiler.ARG_PALNUM;
+import static com.parisoft.supertiler.SuperTiler.ARG_PRIORITY;
+import static com.parisoft.supertiler.SuperTiler.ARG_TILEOFF;
 import static com.parisoft.supertiler.SuperTiler.ARG_TILESET;
+import static com.parisoft.supertiler.SuperTiler.ARG_TILESETNUM;
+import static com.parisoft.supertiler.SuperTiler.ARG_TILESIZE;
+import static com.parisoft.supertiler.SuperTiler.ARG_XOFF;
+import static com.parisoft.supertiler.SuperTiler.ARG_YOFF;
 import static com.parisoft.supertiler.SuperTiler.input;
 import static com.parisoft.supertiler.SuperTiler.metatileHeight;
 import static com.parisoft.supertiler.SuperTiler.metatileWidth;
@@ -44,8 +60,7 @@ public class SpriteCmd implements Cmd {
         }
 
         if (frames.isEmpty()) {
-            System.err.println("No frames");
-            return;
+            throw new IllegalStateException("No metatiles found");
         }
 
         frames.forEach(Frame::createLargeMetatiles);
@@ -99,5 +114,39 @@ public class SpriteCmd implements Cmd {
 
             Files.write(Paths.get(palette), rgb15, CREATE, TRUNCATE_EXISTING);
         }
+    }
+
+    public static void create(ArgumentParser parser) {
+        Subparser sprite = parser.addSubparsers()
+                .addParser("sprite", true)
+                .description("Generates " + ARG_TILESET + ", " + ARG_PALETTE + "s, and " + ARG_METATILE + "s from image")
+                .setDefault("cmd", new SpriteCmd());
+        sprite.addArgument("-i", "--" + ARG_INPUT).nargs("?").required(true).type(String.class).help("Input indexed PNG image");
+        sprite.addArgument("-t", "--" + ARG_TILESET).nargs("?").required(false).type(String.class).help("Output " + ARG_TILESET + " file");
+        sprite.addArgument("-p", "--" + ARG_PALETTE).nargs("?").required(false).type(String.class).help("Output " + ARG_PALETTE + " file");
+        sprite.addArgument("-m", "--" + ARG_METATILE).nargs("?").required(false).type(String.class).help("Prefix for " + ARG_METATILE + " files");
+        sprite.addArgument("-M", "--" + ARG_MODE).nargs("?").required(false).type(String.class).choices("snes", "nes").setDefault("snes").help("Mode for target console");
+        sprite.addArgument("-B", "--" + ARG_BPP).nargs("?").required(false).type(Integer.class).choices(2, 4, 8).setDefault(4).help("Depth or number of colors per pixel");
+        sprite.addArgument("-W", "--" + ARG_METAW).nargs("?").required(false).type(Integer.class).setDefault(8).help("Metatile width in pixels");
+        sprite.addArgument("-H", "--" + ARG_METAH).nargs("?").required(false).type(Integer.class).setDefault(8).help("Metatile height in pixels");
+        sprite.addArgument("-s", "--" + ARG_TILESIZE).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3, 4, 5).setDefault(0)
+                .help("Size of the sprites (SNES only):" + System.lineSeparator()
+                              + "0 =  8x8  and 16x16 sprites" + System.lineSeparator()
+                              + "1 =  8x8  and 32x32 sprites" + System.lineSeparator()
+                              + "2 =  8x8  and 64x64 sprites" + System.lineSeparator()
+                              + "3 = 16x16 and 32x32 sprites" + System.lineSeparator()
+                              + "4 = 16x16 and 64x64 sprites" + System.lineSeparator()
+                              + "5 = 32x32 and 64x64 sprites");
+        sprite.addArgument("-S", "--" + ARG_APPLYSMALL).nargs("?").required(false).type(Boolean.class).setDefault(true)
+                .help("If " + ARG_APPLYLARGE + " is not set, all tiles size are the one define in tileize. See applylarge when both are set. (SNES only)");
+        sprite.addArgument("-L", "--" + ARG_APPLYLARGE).nargs("?").required(false).type(Boolean.class).setDefault(false)
+                .help("When " + ARG_MODE + " is snes, SuperTiler will first scan for large tiles defined in " + ARG_TILESIZE + " then, if " + ARG_APPLYSMALL + " is set, it will replace the large tile for N small tiles if N <= large/small" + System.lineSeparator()
+                              + "When " + ARG_MODE + " is nes, tiles are 8x16");
+        sprite.addArgument("--" + ARG_PALNUM).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3, 4, 5, 6, 7).setDefault(0).help("Object " + ARG_PALETTE + " number");
+        sprite.addArgument("--" + ARG_PRIORITY).nargs("?").required(false).type(Integer.class).choices(0, 1, 2, 3).setDefault(2).help("Object " + ARG_PRIORITY + " (SNES only)");
+        sprite.addArgument("--" + ARG_TILESETNUM).nargs("?").required(false).type(Integer.class).choices(0, 1).setDefault(0).help("Object " + ARG_TILESET + " number (SNES only)");
+        sprite.addArgument("-X", "--" + ARG_XOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object X offset");
+        sprite.addArgument("-Y", "--" + ARG_YOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object Y offset");
+        sprite.addArgument("-T", "--" + ARG_TILEOFF).nargs("?").required(false).type(Integer.class).setDefault(0).help("Object tile offset");
     }
 }
